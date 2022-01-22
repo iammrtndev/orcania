f4eaab843658ea866ce20e9911dfb65f6f9fb223
iammrtndev <68447700+iammrtndev@users.noreply.github.com>
2022-01-22 18:05:36 +0100

    feat: animation showcase

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,29 @@
 <script lang="ts">
   import Background from './Background/Background.svelte'
+  import HeroCard from './HeroCard/HeroCard.svelte'
   import Navbar from './Navbar/Navbar.svelte'
 </script>
 
 <Background />
-<Navbar />
+<div id="center">
+  <HeroCard
+    image="hero.jpg"
+    clan="sylgarden"
+    isRare={true}
+    armor={2}
+    attack={[2, 4]}
+    health={12}
+    magic={3}
+    attributs={['forest', 'sorcerer']}
+  />
+</div>
+
+<style>
+  #center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 100vh;
+  }
+</style>
